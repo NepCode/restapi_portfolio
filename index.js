f2622db6095b452317355d68255f4abdad8b91a3
Jose Antonio Aguila Rojas <aguilantonio16@gmail.com>
2018-10-03 16:28:10 -0500

    change npm script 2

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var app = require('./app'); //conf app express
 var port = 3700;
 
 mongoose.Promise = global.Promise; // realizar conexion con una promesa
-mongoose.connect('mongodb://user:10*nefledeil*10@ds141128.mlab.com:41128/heroku_v3gsm1tz' || 'mongodb://localhost:27017/portafolio', { useNewUrlParser: true })
+mongoose.connect('mongodb://dealer:a123456@ds141128.mlab.com:41128/heroku_v3gsm1tz', { useNewUrlParser: true })
     .then(() => { //comprobar si me he conectado al db
         console.log("DB connection successfully established...");
 
