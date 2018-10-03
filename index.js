b1e403c3879d4e316124730df465b6eb8bc9edae
Jose Antonio Aguila Rojas <aguilantonio16@gmail.com>
2018-10-03 16:36:04 -0500

    change npm script 2

--- index.js
+++ index.js
@@ -4,12 +4,14 @@ var mongoose = require('mongoose'); //cargar o importar modulo mongoose
 var app = require('./app'); //conf app express
 var port = 3700;
 
-var mongoDB = "mongodb://dealer:a123456@ds141128.mlab.com:41128/heroku_v3gsm1tz";
+mongoose.Promise = global.Promise; // realizar conexion con una promesa
+mongoose.connect('mongodb://dealer:a123456@ds141128.mlab.com:41128/heroku_v3gsm1tz', { useNewUrlParser: true })
+    .then(() => { //comprobar si me he conectado al db
+        console.log("DB connection successfully established...");
 
-mongoose.connect(mongoDB, {
-    useMongoClient: true
-});
-
-var db = mongoose.connection;
-
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+        //creacion del servidor
+        app.listen(port, () => {
+            console.log("servidor corriendo correctamente en la url: localhost:3700");
+        });
+    })
+    .catch(err => console.log('no se pudo conectar' + err));
