bad3025dd924b2c8351169ee34bbca038aac6bc0
Jose Antonio Aguila Rojas <aguilantonio16@gmail.com>
2018-10-03 16:09:49 -0500

    change npm script 2

--- index.js
+++ index.js
@@ -5,13 +5,13 @@ var app = require('./app'); //conf app express
 var port = 3700;
 
 mongoose.Promise = global.Promise; // realizar conexion con una promesa
-mongoose.connect('mongodb://localhost:27017/portafolio', {useNewUrlParser: true})
-        .then(() => { //comprobar si me he conectado al db
-            console.log("DB connection successfully established...");
+mongoose.connect(process.env.MONGODB || 'mongodb://localhost:27017/portafolio', { useNewUrlParser: true })
+    .then(() => { //comprobar si me he conectado al db
+        console.log("DB connection successfully established...");
 
-            //creacion del servidor
-            app.listen(port,() => {
-                console.log("servidor corriendo correctamente en la url: localhost:3700");
-            } );
-        })
-        .catch(err => console.log(err));
+        //creacion del servidor
+        app.listen(port, () => {
+            console.log("servidor corriendo correctamente en la url: localhost:3700");
+        });
+    })
+    .catch(err => console.log(err));
