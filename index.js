1bebd8a431623b48d901e6150004f258f2014997
Jose Antonio Aguila Rojas <aguilantonio16@gmail.com>
2018-10-20 23:47:00 -0500

    files updated to work in  local environment

--- index.js
+++ index.js
@@ -5,12 +5,14 @@ var app = require('./app'); //conf app express
 var port = 3700;
 
 mongoose.Promise = global.Promise; // realizar conexion con una promesa
-mongoose.connect('mongodb://dealer:a123456@ds141128.mlab.com:41128/heroku_v3gsm1tz' || 'mongodb://localhost:27017/portafolio', { useNewUrlParser: true })
+//mongoose.connect('mongodb://dealer:a123456@ds141128.mlab.com:41128/heroku_v3gsm1tz' || 'mongodb://localhost:27017/portafolio', { useNewUrlParser: true })
+mongoose.connect('mongodb://localhost:27017/portafolio', { useNewUrlParser: true })
     .then(() => { //comprobar si me he conectado al db
         console.log("DB connection successfully established...");
 
         //creacion del servidor
-        app.listen(process.env.PORT || 5000 || port, () => {
+        //app.listen(process.env.PORT || 5000 || port, () => {
+        app.listen( port, () => {
             console.log("servidor corriendo correctamente en la url: localhost:3700");
         });
     })
